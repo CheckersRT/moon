1ce80f2dda43f18c868f106b5e1644ccd2d76903
Tom Dent <tomdent991@hotmail.com>
2025-03-07 22:50:17 +0100

    satellite and bloom

---
+++ satelite.js
@@ -0,0 +1,105 @@
+import * as THREE from "three"
+import GUI from "lil-gui"
+
+export default class Satelite extends THREE.Object3D {
+
+    constructor(gui) {
+        super()
+        this.gui = gui
+        this.solarCells = new SolarCells(this.gui)
+        this.satBody = new SatBody(this.gui)
+        this.fakeSun = new FakeSun()
+        this.add(this.solarCells, this.satBody, this.fakeSun)
+        this.position.z = 12
+        this.position.x = 3
+        this.scale.set(0.6, 0.6, 0.6)
+        return this
+    }
+}
+
+class SolarCells extends THREE.Object3D {
+    constructor(gui) {
+        super()
+        this.textureLoader = new THREE.TextureLoader()
+        this.loadTextures()
+
+        this.geometry = new THREE.PlaneGeometry(3, 0.5, 16, 4)
+        this.material = new THREE.MeshBasicMaterial({map: this.colorTexture, side: THREE.DoubleSide})
+        this.mesh = new THREE.Mesh(this.geometry, this.material)
+        this.add(this.mesh)
+        return this
+    }
+    
+    loadTextures() {
+        this.colorTexture = this.textureLoader.load("solar_cells/small/SolarCells_512_albedo.png")
+        this.colorTexture.colorSpace = THREE.SRGBColorSpace
+        this.colorTexture.repeat.set(3, 0.6)
+        this.colorTexture.wrapS = THREE.RepeatWrapping
+    }
+}
+
+class SatBody extends THREE.Object3D {
+    constructor(gui) {
+        super()
+        this.textureLoader = new THREE.TextureLoader()
+        this.params = {
+            displacementScale: 0.0001,
+            normalScale: 0.5,
+            aoIntensity: 1.25,
+            metalness: 1.25,
+            roughness: 0.5,
+            repeat: {x: 0.6, y: 0.7}
+        }
+
+        this.colorTexture = this.textureLoader.load("scifi_panel/small/Scifi_Panel_512_albedo.png")
+        this.colorTexture.colorSpace = THREE.SRGBColorSpace
+        this.colorTexture.repeat.set(this.params.repeat.x, this.params.repeat.y)
+        this.colorTexture.wrapS = THREE.RepeatWrapping
+
+        this.aoTexture = this.textureLoader.load("scifi_panel/small/Scifi_Panel_512_ao.png")
+        this.aoTexture.repeat.set(this.params.repeat.x, this.params.repeat.y)
+
+        this.heightTexture = this.textureLoader.load("scifi_panel/small/Scifi_Panel_512_height.png")
+        this.heightTexture.repeat.set(this.params.repeat.x, this.params.repeat.y)
+
+        this.normalTexture = this.textureLoader.load("scifi_panel/small/Scifi_Panel_512_normal.png")
+        this.normalTexture.repeat.set(this.params.repeat.x, this.params.repeat.y)
+
+        this.metallicTexture = this.textureLoader.load("scifi_panel/small/Scifi_Panel_512_metallic.png")
+        this.metallicTexture.repeat.set(this.params.repeat.x, this.params.repeat.y)
+
+        this.roughnessTexture = this.textureLoader.load("scifi_panel/small/Scifi_Panel_512_roughness.png")
+        this.roughnessTexture.repeat.set(this.params.repeat.x, this.params.repeat.y)
+
+        this.geometry = new THREE.BoxGeometry(0.7, 0.6, 0.5, 16, 16)
+        this.material = new THREE.MeshStandardMaterial({map: this.colorTexture, aoMap: this.aoTexture, displacementMap: this.heightTexture, normalMap: this.normalTexture, metalnessMap: this.metallicTexture, roughnessMap: this.roughnessTexture })
+        this.material.displacementScale = this.params.displacementScale
+        this.material.normalScale.set(0.5, 0.5)
+        this.material.aoMapIntensity = this.params.aoIntensity
+        this.material.metalness = this.params.metalness
+        this.material.roughness = this.params.roughness
+        
+
+        this.mesh = new THREE.Mesh(this.geometry, this.material)
+
+        const satBodyDir = gui.addFolder("Satelite body")
+
+        satBodyDir.add(this.material, "aoMapIntensity").min(0.1).max(2).step(0.001)
+        satBodyDir.add(this.material, "metalness").min(0.1).max(2).step(0.001)
+        satBodyDir.add(this.material, "roughness").min(0.1).max(2).step(0.001)
+
+        this.add(this.mesh)
+        this.position.z = -0.3
+        return this
+    } 
+}
+
+class FakeSun extends THREE.Object3D {
+    constructor() {
+        super()
+        this.position.set(20, 5, 30)
+        this.name = "fakeSun"
+
+        return this
+    }
+}
--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/Addons.js"
 import GUI from "lil-gui"
+import { EffectComposer } from "three/examples/jsm/Addons.js"
+import { RenderPass } from "three/examples/jsm/Addons.js"
+import { UnrealBloomPass } from "three/examples/jsm/Addons.js"
+import { AfterimagePass } from "three/examples/jsm/Addons.js"
+import { OutputPass } from "three/examples/jsm/Addons.js"
+import { ShaderPass } from "three/examples/jsm/Addons.js"
+import Satelite from "./satelite"
 
 const canvas = document.querySelector("canvas.webgl")
 const width = window.innerWidth, height = window.innerHeight
@@ -11,7 +18,7 @@ export default class Moon {
         // this.scene.background = new THREE.Color(0x00000)
         
         this.camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 1000)
-        this.camera.position.z = 15
+        this.camera.position.z = 22
         this.camera.position.y = 0
         this.scene.add(this.camera)
         
@@ -19,44 +26,138 @@ export default class Moon {
         this.textures = this.loadTextures()
         this.scene.background = this.nightSky
         
-        this.ambientLight = new THREE.AmbientLight(0xffffff, 2)
-        this.pointLight = new THREE.PointLight(0xffffff, 4)
-        this.pointLight.position.z = 5
-        this.scene.add(this.ambientLight, this.pointLight)
+        this.ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+        this.directionalLight = new THREE.DirectionalLight(0xffffff, 2)
+        this.directionalLight.position.set(20, 5, 30)
+        this.scene.add(
+            this.ambientLight, 
+            this.directionalLight,
+        )
         
         this.gui = this.setUpGui()
-        
-        this.stars = this.addStars()
-        console.log("stars", this.stars)
-        this.scene.add(...this.stars)
-        this.moon = this.addMoon()
-        this.scene.add(this.moon)
-        
+
+
         this.controls = new OrbitControls(this.camera, canvas)
         this.controls.maxPolarAngle = Math.PI /2
         this.controls.minPolarAngle = Math.PI /2
         this.controls.maxDistance = 22
-        this.controls.minDistance = 4
+        this.controls.minDistance = 1
 
         this.renderer = new THREE.WebGLRenderer({canvas: options.canvas})
         this.renderer.setSize(width, height)
         this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        this.renderer.toneMapping = THREE.CineonToneMapping
+        this.renderer.toneMappingExposure = 1
+        this.renderer.outputColorSpace = THREE.SRGBColorSpace
         this.renderer.setAnimationLoop(this.animate.bind(this))
+        
+        this.renderScene = new RenderPass(this.scene, this.camera)
+        this.bloomPass = new UnrealBloomPass(new THREE.Vector2(width, height), 1, 0.5, 0.25)
+        
+        this.bloomComposer = new EffectComposer(this.renderer)
+        this.bloomComposer.addPass(this.renderScene)
+        this.bloomComposer.addPass(this.bloomPass)
+        this.bloomComposer.renderToScreen = false
+        this.bloomParams = {
+            threshold: 1,
+			strength: 0.2,
+			radius: 0,
+			exposure: 1
+        }
+        this.bloomPass.threshold = this.bloomParams.threshold
+        this.bloomPass.strength = this.bloomParams.strength
+        this.bloomPass.radius = this.bloomParams.radius
 
+        const bloomDir = this.gui.addFolder("Bloom")
+        bloomDir.add(this.bloomParams, "threshold", 0.0, 1.0).onChange((value) => {
+            this.bloomPass.threshold = Number(value)
+        })
+        bloomDir.add(this.bloomParams, "strength", 0.0, 2.0).onChange((value) => {
+            this.bloomPass.strength = Number(value)
+        })
+        bloomDir.add(this.bloomParams, "radius", 0.0, 2.0).onChange((value) => {
+            this.bloomPass.radius = Number(value)
+        })
+        
+        this.mixPass = new ShaderPass(
+            new THREE.ShaderMaterial({
+                uniforms: {
+                    baseTexture: {value: null},
+                    bloomTexture: {value: this.bloomComposer.renderTarget2.texture}
+                }, 
+                vertexShader: document.getElementById("vertexshader").textContent,
+                fragmentShader: document.getElementById("fragmentshader").textContent
+            }), "baseTexture"
+        )
+        
+        this.finalComposer = new EffectComposer(this.renderer)
+        this.finalComposer.addPass(this.renderScene)
+        this.finalComposer.addPass(this.mixPass)
+        
+        this.outputPass = new OutputPass()
+        this.finalComposer.addPass(this.outputPass)
+        
+        this.BLOOM_SCENE = 1
+        this.bloomLayer = new THREE.Layers()
+        this.bloomLayer.set(this.BLOOM_SCENE)
+
+        this.nonBloomed = this.nonBloomed.bind(this)
+        this.restoreMaterial = this.restoreMaterial.bind(this)
+
+        
+        this.darkMaterial = new THREE.MeshBasicMaterial({color: 0x000000})
+        this.materials = []
+        
+        this.stars = this.addStars()
+        this.scene.add(...this.stars)
+        this.moon = this.addMoon()
+        this.scene.add(this.moon)
+        this.satelite = new Satelite(this.gui)
+        this.scene.add(this.satelite)
         this.resize()
+    }
+
+    nonBloomed(object) {
+        if(object.isMesh && this.bloomLayer.test(object.layers) === false) {
+            this.materials[object.uuid] = object.material
+            object.material = this.darkMaterial
+        }
+    }
+
+    restoreMaterial(object) {
+        if(this.materials[object.uuid]) {
+            object.material = this.materials[object.uuid]
+            delete this.materials[object.uuid]
+        }
     }
 
     animate(time) {
 
-        this.moon.rotation.y = time/10000
+        this.moon.rotation.y = time/50000
 
-        this.renderer.render(this.scene, this.camera)
+        const orbitRadius = 10 // Adjust orbit distance
+        const orbitSpeed = 0.0002 // Adjust speed of orbit
+        const angle = time * orbitSpeed // Angle based on time
+    
+        this.satelite.position.x = this.moon.position.x + orbitRadius * Math.cos(angle) * 2
+        this.satelite.position.z = this.moon.position.z + orbitRadius * Math.sin(angle)
+        this.satelite.position.y = this.moon.position.y + orbitRadius * Math.sin(angle) / 2
+
+        this.satelite.lookAt(this.satelite.children.find((child) => child.name === "fakeSun").position)
+
+        this.scene.traverse(this.nonBloomed)
+
+        this.bloomComposer.render()
+
+        this.scene.traverse(this.restoreMaterial)
+
+        this.finalComposer.render()
     }
 
     addMoon() {
         this.moonParams = {
-            metalness: 0.1,
-            roughness: 2,
+            metalness: 0.5,
+            roughness: 0.8,
             displacementScale: 0.05,
         }
         
@@ -89,11 +190,14 @@ export default class Moon {
 
         const stars = []
 
-        const yellowStars = createYellowStars(100)
+        const yellowStars = createYellowStars(80)
         const redStars = createRedStars(70)
-        const whiteStars = createWhiteStars(100)
+        const whiteStars = createWhiteStars(80)
 
         stars.push(...yellowStars, ...redStars, ...whiteStars)
+        stars.forEach((star) => {
+            star.layers.enable(this.BLOOM_SCENE)
+        })
 
         return stars
     }
@@ -111,6 +215,8 @@ export default class Moon {
        this.nightSky = this.textureLoader.load("/nightsky.jpg")
        this.nightSky.colorSpace = THREE.SRGBColorSpace
 
+
+
     }
 
     setUpGui() {
@@ -122,10 +228,12 @@ export default class Moon {
 
     resize() {
         window.addEventListener("resize", (event) => {
-            this.renderer.setSize(window.innerWidth, window.innerWidth)
-            this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
             this.camera.aspect = window.innerWidth / window.innerHeight
             this.camera.updateProjectionMatrix()
+            this.renderer.setSize(window.innerWidth, window.innerHeight)
+            this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+            this.bloomComposer.setSize(window.innerWidth, window.innerHeight)
+            this.finalComposer.setSize(window.innerWidth, window.innerHeight)
         })
     }
 }
